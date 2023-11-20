d7d723406421b8a19f4e136cdacf65d188a310e1
Gal Gueta <galg@cynet.com>
2023-11-20 21:11:09 +0200

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import qwikdev from "@qwikdev/astro";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev(), tailwind()]
+  integrations: [qwikdev(), tailwind()],
+  output: "server",
+  adapter: vercel(),
 });
